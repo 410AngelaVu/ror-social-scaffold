6245f701d735a606cec0fa4407bb5ee08983550a
410AngelaVu <andjela1993@gmail.com>
2021-01-20 17:44:58 +0100

    Added button to invite user to be friends, and added UserHelper

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,44 +1,46 @@
 class FriendshipsController < ApplicationController
 
-def new
-@friendship = Friendship.new
-end
+ def new
+    @friendship = Friendship.new
+  end
 
-def create
-@friendship = current_user.friendships.build(params_friendship)
-if @friendship.save
-	flash[:notice] = 'Friendship was saved!'
-	redirect_back(fallback_location: new_user_friendship_path)
-else
-	flash[:notice] = 'Friendship was not saved'
-	render :create
-	end
+  def create
+    @friendship = current_user.friendships.build(params_friendship)
+    if @friendship.save
+      flash[:notice] = 'Friendship was saved correctly.'
+      redirect_back(fallback_location: new_user_friendship_path)
+    else
+      render :create
+    end
+  end
 
-def update 
-friend = User.find(params[:id])
-friendship = friend.friendships.find_by(friend_id: current_user.id)
-if current_user.friend_requests.include?(friend)
-	friendship.confirm_friend
-	flash[:notice] = 'Friendship is confirmed'
-	redirect_back(fallback_location: user_path)
-end
-end
+  def update
+    friend = User.find(params[:id])
+    friendship = friend.friendships.find_by(friend_id: current_user.id)
+    if current_user.friend_requests.include?(friend)
+      friendship.confirm_friend
+      flash[:notice] = 'Friendship was confirmed correctly.'
+      redirect_back(fallback_location: user_path)
+    else
+      render :update
+    end
+  end
 
-def destroy
-friend = User.find(params[:id])
-friendship = current_user.friendships.find_by(friend_id: current_user.id)
+  def destroy
+    friend = User.find(params[:id])
+    friendship = friend.friendships.find_by(friend_id: current_user.id)
 if friendship.destroy
 	flash[:notice] = 'Friendship was rejected'
 else
 	flash[:notice] = 'Something went wrong'
 end
-redirect_back(fallback_location: user_path)
-end
+    redirect_back(fallback_location: user_path)
+  end
 
-private
+  private
 
-def params_friendship
-params.require(:friendship).permit(:friend_id)
-end
+  def params_friendship
+    params.require(:friendship).permit(:friend_id)
+  end
 
 end
